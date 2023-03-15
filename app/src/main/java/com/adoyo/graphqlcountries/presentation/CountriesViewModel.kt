1a460f70f7b903d5152e85e07e3a6a5e8a2aed06
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-15 06:37:42 +0300

    initial commit

---
+++ app/src/main/java/com/adoyo/graphqlcountries/presentation/CountriesViewModel.kt
@@ -0,0 +1,45 @@
+package com.adoyo.graphqlcountries.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.adoyo.graphqlcountries.domain.DetailedCountry
+import com.adoyo.graphqlcountries.domain.GetCountriesUseCase
+import com.adoyo.graphqlcountries.domain.GetCountryUseCase
+import com.adoyo.graphqlcountries.domain.SimpleCountry
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CountriesViewModel(
+    private val getCountriesUseCase: GetCountriesUseCase,
+    private val getCountryUseCase: GetCountryUseCase
+): ViewModel() {
+    private val _state = MutableStateFlow(CountryState())
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+            _state.update { it.copy(countries = getCountriesUseCase.execute(),isLoading = false) }
+        }
+    }
+
+    fun selectCountry(code: String) {
+        viewModelScope.launch {
+            _state.update { it.copy(
+                selectedCountry = getCountryUseCase.execute(code)
+            ) }
+        }
+    }
+
+    fun dismissDialog() {
+        _state.update { it.copy(selectedCountry = null) }
+    }
+
+    data class CountryState(
+        val countries: List<SimpleCountry> = emptyList(),
+        val isLoading: Boolean = false,
+        val selectedCountry: DetailedCountry? = null
+    )
+}
